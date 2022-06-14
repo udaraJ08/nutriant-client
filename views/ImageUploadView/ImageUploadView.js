337b55e5866815aca3dba68ce8d8c62e7e745d4a
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-06-14 12:52:38 +0530

    image upload view done

--- views/ImageUploadView/ImageUploadView.js
+++ views/ImageUploadView/ImageUploadView.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {Box, Image, StatusBar} from 'native-base';
 import fallBack from '../../assets/utils-images/img_1.png';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const ImageUploadView = ({navigation}) => {
+  //States
+  const [image, setImage] = useState(
+    'https://media.istockphoto.com/vectors/fresh-mango-and-a-half-of-mango-fruit-transparent-vector-vector-id542828310?b=1&k=20&m=542828310&s=612x612&w=0&h=ZDvETGS2kXvW2lPqiS-sgtQDMmo3pe9U5j6F1pIkUdA=',
+  );
+
   //routes
   const navigateToArchive = () => {
     navigation.navigate('archive');
+  };
+
+  //Common functions
+  const openGallery = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true,
+      includeBase64: true,
+    })
+      .then(image => {
+        setImage(`data:${image.mime};base64,${image.data}`);
+        console.log(image);
+      })
+      .catch(err => {
+        alert(err.message);
+      });
+  };
+
+  const openCamera = () => {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+      includeBase64: true,
+    })
+      .then(image => {
+        setImage(`data:${image.mime};base64,${image.data}`);
+      })
+      .catch(err => {
+        alert(err.message);
+      });
   };
 
   return (
@@ -19,30 +57,29 @@ const ImageUploadView = ({navigation}) => {
       />
       <Box style={style.mainContainer}>
         <Box style={style.innerContainer}>
-          {' '}
           <Box style={[style.topic]}>
             <Text style={[style.fontSmall, style.textTopic]}>Upload image</Text>
           </Box>
           <Box style={[style.imageUpload, style.center]}>
             <Image
               style={[style.imageUp]}
-              source={fallBack}
+              source={{uri: image}}
               alt="fallback-image"
             />
           </Box>
           <Box style={[style.typeContainer]}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={openCamera}>
               <Box style={[style.imageBtn, style.takeImage]}>
                 <Icon name="camera" size={30} color="white" />
               </Box>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={openGallery}>
               <Box style={[style.imageBtn, style.galleryImageBtn]}>
                 <Icon name="image" size={30} color="white" />
               </Box>
             </TouchableOpacity>
           </Box>
-          <TouchableOpacity c>
+          <TouchableOpacity>
             <Box style={[style.searchBtn, style.center]}>
               <Text
                 style={{
